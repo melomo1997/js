a05ae5ec9c5ecaebef14ffe3f8ef261ec017feb1
Your Name <you@example.com>
2023-10-27 20:19:14 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// create an account object
+
+const account = {
+   
+    name: "Cactus",
+  };
+  
+ 
+  
+ // create a function called menu()
+
+  function menu() {
+  
+    const choice = parseFloat(
+      prompt(
+        "Please select from the menu:\n1) Add income\n2) Add expense\n3)List all expenses\n4) See total balance"
+      )
+    );
+  
+    console.log("Choice: " + choice);
+    const amount = parseFloat(prompt("How much was your incom?")("How much was your expence?"));
+
+    
+
+   
+
+  
+
+   
+    
+  }
+  
+  menu();
+ 
