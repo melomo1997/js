19f6e40ca1691582a1ffe96d579e0862a1a71101
Your Name <you@example.com>
2023-11-12 16:15:52 +0100

    sofar

--- script.js
+++ script.js
@@ -1,35 +1,75 @@
 
-// create an account object
 
-const account = {
-   
-    name: "Cactus",
-  };
-  
- 
-  
- // create a function called menu()
 
-  function menu() {
-  
-    const choice = parseFloat(
-      prompt(
-        "Please select from the menu:\n1) Add income\n2) Add expense\n3)List all expenses\n4) See total balance"
-      )
-    );
-  
-    console.log("Choice: " + choice);
-    const amount = parseFloat(prompt("How much was your incom?")("How much was your expence?"));
+// TO DO LIST
+// 1-CREAT AN OBJECT FOR YOUR WEBSITE
 
-    
+const cactusObject = {
+    name:"Cactus",
+    expenses:[],
+    income:[],
+    function addExpenses (){
+        prompt(
+            'what was your expense?(rent/gas/food etc)'
+            )
 
-   
+    }
+}
+ console.log(cactusObject);
 
-  
 
-   
-    
-  }
-  
-  menu();
- 
+
+// 2-CREAT A PROMP
+function menu(){
+    const question =parseFloat(
+        prompt(
+            'Please select from the menu: \n1) Add Income\n2) Add Expences\n3) List All Expences\n4) Get Summery'
+        )
+    )
+};
+
+switch(choices) {
+    case 1:
+        prompt(
+            "add your incom"
+        )
+        break;
+    case 2:
+            prompt(
+                "add your expences"
+            )
+            break;
+    case 3:
+            prompt(
+                "what was your expences?(gas/food/rent)"
+            )
+            break;
+    default:
+}
+
+
+const info = prompt("Please select from the menu: \n1) Add Income\n2) Add Expenses\n3) List All Expenses\n4) Get Summary");
+
+switch (info) {
+  case "1":
+    prompt("You selected: Add Income");
+    // Perform the necessary actions for adding income
+    break;
+  case "2":
+    prompt("You selected: Add Expenses");
+    // Perform the necessary actions for adding expenses
+    break;
+  case "3":
+    prompt("You selected: List All Expenses");
+    // Perform the necessary actions for listing all expenses
+    break;
+  case "4":
+    prompt("You selected: Get Summary");
+    // Perform the necessary actions for getting the summary
+    break;
+  default:
+    prompt("Invalid selection");
+    break;
+}
+
+
